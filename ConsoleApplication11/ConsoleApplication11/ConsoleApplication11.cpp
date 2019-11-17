0fb379357988181c06293767e66d5b8349fc9dd4
shizikang <1142752699@qq.com>
2019-11-17 17:33:08 +0800

    c.11

---
+++ ConsoleApplication11/ConsoleApplication11/ConsoleApplication11.cpp
@@ -0,0 +1,34 @@
+﻿//#include<stdio.h>
+//int main()
+//{
+//	int  j,  i;
+//	int m;
+//	for (i = 1;i <=1000;i++)
+//	{
+//		m = 0;
+//		for (j = 1;j < i;j++)
+//		{ 
+//			if (i%j==0)
+//			{
+//				m = m + j;
+//			}
+//		}
+//		if (i == m)
+//		{
+//			printf("%d\n", i);
+//		}
+//	}
+//	return 0;
+//}
+
+
+#include <stdio.h>
+int main()
+{
+	int a;
+	scanf_s("%d", &a);
+	for (int i = 2;i <= a;i=i*2) {
+		if (a == i)
+			printf("yes");
+	}
+}
