96411ac0e5587c6a55c02776c7018a46643706e0
shizikang <1142752699@qq.com>
2019-11-18 16:55:46 +0800

    frist

---
+++ 找寻数组最大值最小值.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+int main()
+{
+	int a[6];
+	int j,i;
+	int t;
+	for ( i = 0;i < 6;i++)
+	{
+		scanf_s("%d", &a[i]);
+	}
+	for (i = 0;i < 6;i++) {
+		for (j = 0;j < 6- i-1;j++)
+		{
+			if (a[j] < a[j + 1]) {
+				t = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = t;
+			}
+		}
+	}
+	printf("最大是%d", a[0]);
+	printf("最小是%d", a[5]);
+	return 0;
+}
