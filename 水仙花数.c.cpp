b6316a0cbda8e9a12c1bf1a8c2e7df543e27d26d
shizikang <1142752699@qq.com>
2019-11-18 16:52:15 +0800

    first

---
+++ 水仙花数.c.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+int main()
+{
+	int a, b, c, i;
+	for (i = 100;i < 1000;i++)
+	{
+		a = i % 10;
+		b = (i / 10) % 10;
+		c = i / 100;
+		if (i == a * a * a + b * b * b + c * c * c)
+			printf("%d\n", i);
+	}
+	return 0;
+}
