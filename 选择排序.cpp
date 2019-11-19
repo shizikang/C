beba84fb98a92790a280fa935446cdd75951e200
shizikang <1142752699@qq.com>
2019-11-19 11:42:46 +0800

    frist

---
+++ 选择排序.cpp
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+int main()
+{
+	int j, i, k, t;
+	int a[6] = { 2,3,8,7,4,5 };
+	for (i = 0;i < 5;i++) {
+		k = i;
+			for (j = i + 1;j < 6;j++) {
+				if (a[k] < a[j]) {
+					k = j;
+				}
+			}
+			if (a[k] != a[i]) {
+				t = a[k];
+				a[k] = a[i];
+				a[i] = t;
+			}
+	}
+	for (i = 0;i < 6;i++) {
+		printf("%d\n", a[i]);
+	}
+	return 0;
+}
