1f85b51f2ebf6803ce5b8681fd99e106aff1c40c
shizikang <1142752699@qq.com>
2019-11-26 21:38:59 +0800

    ffirst

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+int main()
+{
+	int a, b, c;
+	scanf_s("%d%d%d", &a, &b, &c);
+	if (a + b > c) {
+		if (a + c > b) {
+			if (c + b > a) {
+				if (a * a == b * b + c * c || b * b == a * a + c * c || c * c == a * a + b * b) {
+					printf("yes\n");
+				}
+			}
+		}
+	}
+}
